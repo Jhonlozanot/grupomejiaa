cf0834e3fe585e331c64264b214b5b0d5bc29aa2
Jhonlozanot <84853141+Jhonlozanot@users.noreply.github.com>
2022-12-05 20:39:03 -0400

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -56,7 +56,7 @@ const Home = (data) => {
   );
 };
 
-Home.getInitialProps = async () => {
+/*Home.getInitialProps = async () => {
   const response = await fetch(
     "https://backend-grupo-mejia.herokuapp.com/api/asociado"
   );
@@ -64,6 +64,6 @@ Home.getInitialProps = async () => {
   const body = await response.json();
 
   return { data: body };
-};
+};*/
 
 export default Home;
